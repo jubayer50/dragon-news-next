a62094f8394b06e8139ee8bfec93ed1f3bb27116
jubayer50 <aujubayer1@gmail.com>
2026-04-26 00:26:08 +0600

    loading added

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+const LoadingPage = () => {
+  return (
+    <div className="h-[75vh] flex justify-center items-center">
+      <h2 className="font-medium text-lg">Loading...</h2>
+    </div>
+  );
+};
+
+export default LoadingPage;
