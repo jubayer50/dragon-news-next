362071666428984c9edaf03c332e92a3c157987b
jubayer50 <aujubayer1@gmail.com>
2026-04-26 16:13:06 +0600

    react hook form emplemented with basic valodation

--- src/app/(Auth)/register/page.jsx
+++ src/app/(Auth)/register/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 const RegisterPage = () => {
   return (
     <div className="min-h-screen">
@@ -7,64 +9,69 @@ const RegisterPage = () => {
             Register your account
           </h2>
 
-          <div className="pt-4 md:pt-6 border-t border-[#E7E7E7] space-y-2">
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend font-semibold text-lg md:text-xl">
-                Your Name
-              </legend>
-              <input
-                type="text"
-                className="input w-full"
-                placeholder="Type your Name"
-              />
-            </fieldset>
+          <form onSubmit={""}>
+            <div className="pt-4 md:pt-6 border-t border-[#E7E7E7] space-y-2">
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend font-semibold text-lg md:text-xl">
+                  Your Name
+                </legend>
+                <input
+                  type="text"
+                  className="input w-full"
+                  placeholder="Type your Name"
+                />
+              </fieldset>
 
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend font-semibold text-lg md:text-xl">
-                Photo URL
-              </legend>
-              <input
-                type="text"
-                className="input w-full"
-                placeholder="Type your Photo link"
-              />
-            </fieldset>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend font-semibold text-lg md:text-xl">
+                  Photo URL
+                </legend>
+                <input
+                  type="text"
+                  className="input w-full"
+                  placeholder="Type your Photo link"
+                />
+              </fieldset>
 
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend font-semibold text-lg md:text-xl">
-                Email
-              </legend>
-              <input
-                type="email"
-                className="input w-full"
-                placeholder="Type your Email"
-              />
-            </fieldset>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend font-semibold text-lg md:text-xl">
+                  Email
+                </legend>
+                <input
+                  type="email"
+                  className="input w-full"
+                  placeholder="Type your Email"
+                />
+              </fieldset>
 
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend font-semibold text-lg md:text-xl">
-                Password
-              </legend>
-              <input
-                type="password"
-                className="input w-full"
-                placeholder="Type your Password"
-              />
-            </fieldset>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend font-semibold text-lg md:text-xl">
+                  Password
+                </legend>
+                <input
+                  type="password"
+                  className="input w-full"
+                  placeholder="Type your Password"
+                />
+              </fieldset>
 
-            <div className="flex gap-3 items-center">
-              <input type="checkbox" className="checkbox" />
-              <legend className="fieldset-legend text-[#706F6F]">
-                <span className="font-normal">Accept</span> Term & Condition
-              </legend>
+              <div className="flex gap-3 items-center">
+                <input type="checkbox" className="checkbox" />
+                <legend className="fieldset-legend text-[#706F6F]">
+                  <span className="font-normal">Accept</span> Term & Condition
+                </legend>
+              </div>
             </div>
-          </div>
 
-          <div className="my-5">
-            <button className="btn text-lg md:text-xl font-semibold w-full bg-[#403F3F] text-white">
-              Register
-            </button>
-          </div>
+            <div className="my-5">
+              <button
+                type="submit"
+                className="btn text-lg md:text-xl font-semibold w-full bg-[#403F3F] text-white"
+              >
+                Register
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/app/(Auth)/signin/page.jsx
+++ src/app/(Auth)/signin/page.jsx
@@ -1,6 +1,17 @@
+"use client";
+
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 const SignInPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleLoginBtn = (data) => {};
+
   return (
     <div className="h-screen">
       <div className="max-w-285 mx-auto my-12 flex justify-center items-center">
@@ -9,35 +20,52 @@ const SignInPage = () => {
             Login your account
           </h2>
 
-          <div className="pt-4 md:pt-6 border-t border-[#E7E7E7] space-y-2">
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend font-semibold text-lg md:text-xl">
-                Email Address
-              </legend>
-              <input
-                type="text"
-                className="input w-full"
-                placeholder="Type your Email"
-              />
-            </fieldset>
+          <form onSubmit={handleSubmit(handleLoginBtn)}>
+            <div className="pt-4 md:pt-6 border-t border-[#E7E7E7] space-y-2">
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend font-semibold text-lg md:text-xl">
+                  Email Address
+                </legend>
+                <input
+                  type="email"
+                  {...register("email", { required: "Email is required." })}
+                  className="input w-full"
+                  placeholder="Type your Email"
+                />
 
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend font-semibold text-lg md:text-xl">
-                Password
-              </legend>
-              <input
-                type="password"
-                className="input w-full"
-                placeholder="Type your Password"
-              />
-            </fieldset>
-          </div>
+                {errors.email && (
+                  <p className="mt-1 text-red-600">{errors.email.message}</p>
+                )}
+              </fieldset>
 
-          <div className="my-5">
-            <button className="btn text-lg md:text-xl font-semibold w-full bg-[#403F3F] text-white">
-              Login
-            </button>
-          </div>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend font-semibold text-lg md:text-xl">
+                  Password
+                </legend>
+                <input
+                  type="password"
+                  {...register("password", {
+                    required: "Password is required",
+                  })}
+                  className="input w-full"
+                  placeholder="Type your Password"
+                />
+
+                {errors.password && (
+                  <p className="mt-1 text-red-600">{errors.password.message}</p>
+                )}
+              </fieldset>
+            </div>
+
+            <div className="my-5">
+              <button
+                type="submit"
+                className="btn text-lg md:text-xl font-semibold w-full bg-[#403F3F] text-white"
+              >
+                Login
+              </button>
+            </div>
+          </form>
 
           <p className="text-[#706F6F] font-semibold text-center">
             Dont’t Have An Account ?{" "}
