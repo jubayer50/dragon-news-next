b1d9009362a3f0550cd7fccad959358f5da539d5
jubayer50 <aujubayer1@gmail.com>
2026-04-27 13:29:16 +0600

    proxy applied

---
+++ src/app/(Main)/career/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CareerPage = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-medium text-center mt-10">Career page</h2>
+    </div>
+  );
+};
+
+export default CareerPage;
